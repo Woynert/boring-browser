c369a1be86552ea08e4f8eac33134ff44b9ecd98
woynert <woynertgamer@gmail.com>
2025-04-14 13:19:30 -0500

    feat: load js script to hide media

---
+++ app/src/main/assets/script.js
@@ -0,0 +1,44 @@
+(function f() {
+
+/* Styling */
+
+let eHead = document.getElementsByTagName("head")[0];
+let eStyle = document.createElement('style');
+eHead.appendChild(eStyle);
+eStyle.type = 'text/css';
+
+let css = `
+img, video {
+  visibility: hidden !important;
+}
+
+* {
+  background-image: none !important;
+}
+`;
+
+eStyle.textContent = css;
+
+/* Try to remove some JS */
+
+let r = document.getElementsByTagName('script');
+for (let i = r.length-1; i >= 0; i--) {
+    r[i].remove();
+}
+
+/* Remove iframes */
+
+const TIMEOUT_MS = 3000;
+const loop = function () {
+
+    let r = document.getElementsByTagName('iframe');
+    for (let i = r.length-1; i >= 0; i--) {
+        r[i].remove();
+    }
+    
+    setTimeout(loop, TIMEOUT_MS);
+};
+setTimeout(loop, TIMEOUT_MS);
+loop();
+
+})()
